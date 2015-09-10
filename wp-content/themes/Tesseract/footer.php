aa9cac69e71cad46b8cc7722b08037bd66ccda3f
nb341153 <nik.bo@hotmail.de>
2015-09-10 16:59:01 +0200

    Anzeige Theme by Tyler Moore gelÃöscht

--- wp-content/themes/Tesseract/footer.php
+++ wp-content/themes/Tesseract/footer.php
@@ -65,9 +65,6 @@
                                                 
            			</div><!-- EOF horizontal-menu-wrap -->                       
             
-            <div id="designer">               
-                <?php printf( __( 'Theme by %s', 'tesseract' ), '<a href="http://tyler.com">Tyler Moore</a>' ); ?>
-            </div>            
             
       	</div>                  
         
